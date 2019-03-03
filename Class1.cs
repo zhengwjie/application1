3e7449ac6d2ac6790d672d6584d5170ef479d59c
zhengwjie <48023590+zhengwjie@users.noreply.github.com>
2019-03-03 18:35:55 +0800

    111

---
+++ Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class Class1
+{
+	public Class1()
+	{
+		//
+		// TODO: Add constructor logic here
+		//
+        public static void main(String []args)
+        {
+            Console.WriteLine("Hello world!");
+        }
+	}
+}
